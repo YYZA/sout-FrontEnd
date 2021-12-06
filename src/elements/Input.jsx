967284f03c385292c5572e31aab8129d802a7c51
YYZA <sacoraa@naver.com>
2021-12-06 21:19:55 +0900

    CSS 수정

--- src/elements/Input.jsx
+++ src/elements/Input.jsx
@@ -1,43 +1,54 @@
-import styled from 'styled-components'
-import Grid from './Grid'
-import Text from './Text'
+import styled from "styled-components";
+import Grid from "./Grid";
+import Text from "./Text";
 
 const Input = (props) => {
-  const { label, multiLine, placeholder } = props
+  const { label, multiLine, placeholder, _onChange } = props;
 
   if (multiLine) {
     return (
       <Grid>
         {label && <Text>{label}</Text>}
-        <TextareaBox rows={10} placeholder={placeholder}></TextareaBox>
+        <TextareaBox
+          rows={10}
+          placeholder={placeholder}
+          onChange={_onChange}
+        ></TextareaBox>
       </Grid>
-    )
+    );
   }
 
   return (
     <>
       <Grid>
         {label && <Text>{label}</Text>}
-        <InputBox placeholder={placeholder}></InputBox>
+        <InputBox placeholder={placeholder} onChange={_onChange}></InputBox>
       </Grid>
     </>
-  )
-}
+  );
+};
 
-Input.defaultProps = {}
+Input.defaultProps = {
+  _onChange: () => {},
+};
 
 const InputBox = styled.input`
-  border: 1px solid #000;
+  border-radius: 5px;
+  margin: 10px 0px;
+  border: 1px solid #ddc6b6;
   width: 100%;
   padding: 16px 10px;
   box-sizing: border-box;
-`
+`;
 const TextareaBox = styled.textarea`
-  border: 3px solid #000;
+  border-radius: 5px;
+  margin: 10px 0px;
+  color: #ddc6b6;
+  border: 2px solid #ddc6b6;
   width: 100%;
   padding: 16px 10px;
   resize: none;
   box-sizing: border-box;
-`
+`;
 
-export default Input
+export default Input;
