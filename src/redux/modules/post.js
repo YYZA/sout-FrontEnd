961c45c5dd002357d205f2ad7035025fc59a01d4
YYZA <sacoraa@naver.com>
2021-12-09 17:49:50 +0900

    에러 수정

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -1,35 +1,35 @@
-import { createAction, handleAction, handleActions } from 'redux-actions'
-import { produce } from 'immer'
-import axios from 'axios'
-import { getCookie } from '../../shared/Cookie'
-import { instance } from '../../shared/api'
+import { createAction, handleAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+import axios from "axios";
+import { getCookie } from "../../shared/Cookie";
+import { instance } from "../../shared/api";
 
-const SET_POST = 'SET_POST'
-const ADD_POST = 'ADD_POST'
-const DELETE_POST = 'DELETE_POST'
-const EDIT_POST = 'EDIT_POST'
+const SET_POST = "SET_POST";
+const ADD_POST = "ADD_POST";
+const DELETE_POST = "DELETE_POST";
+const EDIT_POST = "EDIT_POST";
 
 const setPost = createAction(SET_POST, (post) => ({
   post,
-}))
-const addPost = createAction(ADD_POST, (content, url) => ({ content, url }))
-const deletePost = createAction(DELETE_POST, (post_id) => ({ post_id }))
+}));
+const addPost = createAction(ADD_POST, (content, url) => ({ content, url }));
+const deletePost = createAction(DELETE_POST, (post_id) => ({ post_id }));
 const editPost = createAction(EDIT_POST, (content, url, post_id) => ({
   content,
   url,
   post_id,
-}))
+}));
 
 const initialState = {
   list: [],
-}
-const cookie = getCookie('x_auth')
+};
+const cookie = getCookie("x_auth");
 const addPostDB = (content, url) => {
   return async function (dispatch, getState, { history }) {
-    const cookie = getCookie('x_auth')
+    const cookie = getCookie("x_auth");
     await axios
       .post(
-        'http://localhost:8080/newpost',
+        "http://localhost:8080/newpost",
         { content, url },
         {
           headers: {
@@ -38,14 +38,15 @@ const addPostDB = (content, url) => {
         }
       )
       .then((res) => {
-        dispatch(addPost(content, url))
-      })
-    history.push('/')
-  }
-}
+        console.log(res);
+        dispatch(addPost(res.data));
+      });
+    history.push("/");
+  };
+};
 const deletePostDB = (post_id) => {
   return function (dispatch, getState, { history }) {
-    const cookie = getCookie('x_auth')
+    const cookie = getCookie("x_auth");
     axios
       .delete(`http://localhost:8080/api/${post_id}`, {
         headers: {
@@ -53,14 +54,14 @@ const deletePostDB = (post_id) => {
         },
       })
       .then((res) => {
-        dispatch(deletePost(post_id))
-      })
-  }
-}
+        dispatch(deletePost(post_id));
+      });
+  };
+};
 
-const editPostDB = (content, url, post_id, post) => {
+const editPostDB = (content, url, post_id) => {
   return function (dispatch, getState, { history }) {
-    const cookie = getCookie('x_auth')
+    const cookie = getCookie("x_auth");
     axios
       .put(
         `http://localhost:8080/newpost/${post_id}`,
@@ -72,63 +73,59 @@ const editPostDB = (content, url, post_id, post) => {
         }
       )
       .then((res) => {
-        dispatch(addPost(content, url))
-      })
-  }
-}
+        dispatch(editPost(post_id));
+      });
+  };
+};
 
 const getPostDB = () => {
   return async function (dispatch, getState, { history }) {
     await axios
-      .get('http://localhost:8080/', {
-        params: { page: 0, size: 5 },
+      .get("http://localhost:8080/", {
+        params: { page: 0, size: 4 },
         headers: {
           Authorization: cookie,
         },
       })
       .then((res) => {
-        dispatch(setPost(res.data))
-      })
-  }
-}
+        console.log(res);
+        dispatch(setPost(res.data));
+      });
+  };
+};
 
 export default handleActions(
   {
     [SET_POST]: (state, action) => {
-      return { list: action.payload.post }
+      return { list: action.payload.post };
     },
     [DELETE_POST]: (state, action) =>
       produce(state, (draft) => {
         let idx = draft.list.findIndex(
           (val) => val.id === action.payload.post_id
-        )
-        draft.list.splice(idx, 1)
+        );
+        draft.list.splice(idx, 1);
         // draft.list.push(...action.payload.post);
       }),
     [EDIT_POST]: (state, action) =>
       produce(state, (draft) => {
-        let idx = draft.list.findIndex((p) => p.id === action.payload.post_id)
-        draft.list[idx] = { ...draft.list[idx], ...action.payload.post }
+        let idx = draft.list.findIndex((p) => p.id === action.payload.post_id);
+        draft.list[idx] = { ...draft.list[idx], ...action.payload.post };
+        console.log(draft.list[idx]);
       }),
     [ADD_POST]: (state, action) =>
       produce(state, (draft) => {
-        const newObj = {
-          nickname: 'asd',
-          content: action.payload.content,
-          postId: new Date().getTime(),
-          commentList: [],
-        }
-        draft.list.push(newObj)
+        draft.list.push(action.payload.content);
       }),
   },
   initialState
-)
+);
 
 const actionCreators = {
   addPostDB,
   getPostDB,
   editPostDB,
   deletePostDB,
-}
+};
 
-export { actionCreators }
+export { actionCreators };
