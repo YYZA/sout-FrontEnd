cb6dc02c98225efbe1c51116339df01cab299e76
강태수 <mtae616@naver.com>
2021-12-09 14:05:27 +0900

    add kakao login

--- src/pages/KakaoAuthHandle.jsx
+++ src/pages/KakaoAuthHandle.jsx
@@ -1,16 +1,38 @@
+import axios from 'axios'
 import { useEffect } from 'react'
+import { CircularProgress } from '@mui/material'
+import { Grid } from '../elements'
+import styled from 'styled-components'
 
 const KakaoAuthHandle = (props) => {
-  let code = new URL(window.location.href).searchParams.get('code')
-
-  const kakaoLogin = () => {
-    console.log(code)
-  }
-
   useEffect(() => {
+    let code = new URL(window.location.href).searchParams.get('code')
+    const kakaoLogin = async () => {
+      await axios
+        .get(`http://localhost:8080/user/kakao/callback?code=${code}`)
+        .then((res) => {
+          console.log(res)
+          document.cookie = 'x_auth' + '=' + res.headers.authorization
+          props.history.push('/')
+        })
+    }
     kakaoLogin()
   })
-  return <>loading</>
+  return (
+    <>
+      <Container>
+        <CircularProgress />
+      </Container>
+    </>
+  )
 }
 
 export default KakaoAuthHandle
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
